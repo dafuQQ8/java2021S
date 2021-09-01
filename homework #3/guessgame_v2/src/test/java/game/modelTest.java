e2c1f5cf08acf6252aadc83fa27b45c5993bbc71
renceqq <palerence@gmail.com>
2021-09-01 23:40:31 +0300

    commit

--- homework #3/guessgame_v2/src/test/java/game/modelTest.java
+++ homework #3/guessgame_v2/src/test/java/game/modelTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.*;
@@ -18,39 +20,47 @@ class modelTest {
     }
 
     @RepeatedTest(150)
+    @DisplayName("Randomness test.")
     void testTargedNumberisAlwaysInRange() {
         assertThat(model.targetNumber(0, 100)).isBetween(1, 99);
     }
-
+        
     @Test
-    void testASCIIForRangeSetter() {
-        //given
-        int j = 0;
-        String str = null; 
-        int[] numbers = new int[69];
-        for (int i = 58; i < 127; i++) {
-            numbers[j] = i;
-            str = Character.toString((char) i);
-            //then
-            assertFalse(model.rangeSetter(str));
-            j++;
-        } 
-    }
-    
-    @Test
+    @DisplayName("Legitimacy test.")
     void testIscorrectNumberIsReallyLegit() {
         //given
         model.targetNumber = 57;
         //then
-        assertTrue(model.correctnessProofer(model.targetNumber));
-        assertFalse(model.correctnessProofer(model.targetNumber + 1));
+        assertTrue(model.correctnessProofer(model.targetNumber), () -> "Gives true if input matches targetNumber.");
+        assertFalse(model.correctnessProofer(model.targetNumber + 1), () -> "Gives false if input doesn't match targetNumber.");
+        model.targetNumber = 0;
     }
-    
-    @Test
-    void testMethodIsInRange() {
-        model.inputRecorder[1] = 999;
-        for (int i = 1; i < model.inputRecorder[1]; i++) {
-            assertTrue(model.isInRange(i));
+    @Nested
+    public class RangeTests {
+        @Test
+        @DisplayName("Range setter test.")
+        void testASCIIForRangeSetter() {
+            //given
+            int j = 0;
+            String str = null; 
+            int[] numbers = new int[69];
+            for (int i = 58; i < 127; i++) {
+                numbers[j] = i;
+                str = Character.toString((char) i);
+                //then
+                assertFalse(model.rangeSetter(str), () -> "Gives false in case if rangeSetter didn't accept input." /*Input is restricted for [0-9] only*/);
+                j++;
+            } 
         }
+    
+        @Test
+        @DisplayName("Range test.")
+        void testMethodIsInRange() {
+            model.inputRecorder[1] = 999;
+            for (int i = 1; i < model.inputRecorder[1]; i++) {
+                assertTrue(model.isInRange(i), () -> "Gives true if isInRange method works fine.");
+            }
+            model.inputRecorder[1] = 0;
+        }  
     }
 }
