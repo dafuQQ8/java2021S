cc561a5eeb282f3a9c8928510ebc767767ae92d4
renceqq <palerence@gmail.com>
2021-09-12 22:30:59 +0300

    SOoome mOoore changes by @kagu-tsuchi

--- homework #5/student/src/test/java/student/StudentTest.java
+++ homework #5/student/src/test/java/student/StudentTest.java
@@ -1,5 +1,7 @@
 package student;
 
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.BeforeEach;
@@ -9,21 +11,29 @@ import org.junit.jupiter.api.Test;
 public class StudentTest {
     Student humanoid;
     Student reptiloid;
+    Student jackMa;
 
     @BeforeEach
     void initialise() {
-        humanoid = new Student("Mark", "Zuckerberg", 37,"Facebook");
-        reptiloid = new Student("Mark", "Zuckerberg", 37,"Facebook");
+        humanoid = new Student("Mark", "Zuckerberg", 37,"Facebook, Inc.");
+        reptiloid = new Student("Mark", "Zuckerberg", 37,"Facebook, Inc.");
+        jackMa = new Student("Jack", "Ma", 57,"Alibaba Group");
     }
 
     @Test
     void humanoidMarkEqualsToReptiloidMark() {
-        assertTrue(humanoid.equals(reptiloid));
+        assertAll(
+            () -> assertTrue(humanoid.equals(reptiloid), () -> "Gives true if Mark is actual reptiloid."),
+            () -> assertFalse(humanoid.equals(jackMa), () -> "Gives false if Mark is not Jack Ma")
+        );
     }
 
     @RepeatedTest(100)
     void humanoidMarkHashCodeEqualsToReptiloidMarkHashCode() {
-        assertTrue(humanoid.hashCode() == reptiloid.hashCode());
+        assertAll(
+            () -> assertTrue(humanoid.hashCode() == reptiloid.hashCode(), () -> "Gives true if Mark's hash code is same as reptiloid's."),
+            () -> assertFalse(humanoid.hashCode() == jackMa.hashCode(), () -> "Gives false if Mark's hash code doesn't match Jack Ma's")
+        );
     }
 }
 
