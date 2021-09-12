65f3a6db365ccd84147f155822e1fd843c44f0ec
renceqq <palerence@gmail.com>
2021-09-12 22:18:45 +0300

    light changes

--- homework #5/student/src/test/java/student/StudentTest.java
+++ homework #5/student/src/test/java/student/StudentTest.java
@@ -2,18 +2,26 @@ package student;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 public class StudentTest {
-    Student humanoid = new Student("Mark", "Zuckerberg", 37,"Facebook");
-    Student reptiloid = new Student("Mark", "Zuckerberg", 37,"Facebook");
+    Student humanoid;
+    Student reptiloid;
+
+    @BeforeEach
+    void initialise() {
+        humanoid = new Student("Mark", "Zuckerberg", 37,"Facebook");
+        reptiloid = new Student("Mark", "Zuckerberg", 37,"Facebook");
+    }
 
     @Test
     void humanoidMarkEqualsToReptiloidMark() {
         assertTrue(humanoid.equals(reptiloid));
     }
 
-    @Test
+    @RepeatedTest(100)
     void humanoidMarkHashCodeEqualsToReptiloidMarkHashCode() {
         assertTrue(humanoid.hashCode() == reptiloid.hashCode());
     }
